3622d2b6fbf2d2b1d502869ab97eaf1f7a3aace4
Aravind Kumar <84002440+openoncecode@users.noreply.github.com>
2021-09-04 21:21:35 +0530

    Update mortgage-calculator.java

--- Mortgage-Calculator/src/com/opencodeonce/mortgage-calculator.java
+++ Mortgage-Calculator/src/com/opencodeonce/mortgage-calculator.java
@@ -1,26 +1,57 @@
-package com.opencodeonce;
+package com.opencodeonce;  //change this to your package name
 
-import java.text.*;
-import java.util.*;
+import java.text.NumberFormat;
+import java.util.Scanner;
+import java.lang.*;
+
 public class Main {
+public static void main(String[]args) {
+    Scanner sc=new Scanner(System.in);
 
-    public static void main(String []args){
-        final byte MONTHINYEAR=12;
-        final byte PERCENT=100;
-        Scanner sc=new Scanner(System.in);
-
-        System.out.print("Principal :");
-        int principal=sc.nextInt();
-
-        System.out.print("Annual Interest Rate :");
-        float interest=sc.nextFloat();
-        float monthrate= interest/PERCENT/MONTHINYEAR;
-
-        System.out.print("Period(Years) :");
-        int year=sc.nextInt();
-        float power= (float) Math.pow(1+monthrate,year*MONTHINYEAR);
-        double mortgage = principal*((monthrate*power)/(power-1));
-        String amount=NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortage :"+amount);
+    final byte MONTH=12;
+    final byte PERCENTAGE=100;
+    long principal;
+    float interest;
+    int year;
+    double mortgage;
+    while(true){
+        System.out.print("Principal (1K-1M):");
+        principal= sc.nextLong();
+        if(principal>=1000 && principal<=1000000){
+            System.out.println("Your Principal is:"+principal);
+            break;
+        }
+        System.out.println("Please enter principal between 1K and 1M");
+        }
+    while (true){
+        System.out.print("Annual Rate of Intersest:");
+        interest=sc.nextFloat();
+        if(interest>0 && interest<=30){
+            System.out.println("Your Annual Rate of Interest is:"+interest);
+            break;
     }
+        System.out.println("Please enter interest greater than 0 and lesser than or equal to 30");
+    }
+    while (true){
+        System.out.print("Period(years):");
+        year=sc.nextInt();
+        if(year>0 && year<=30){
+            System.out.println("Your Period(year) is:"+year);
+            break;
+        }
+        System.out.println("Please enter year greater than 0 and lesser than or equal to 30");
+    }
+    
+    //mortage calculation: mortage= principal * ((rate)(1+rate)^noyear) / ((1+rate)^noyear-1)
+    float rate= (interest/PERCENTAGE/MONTH);
+
+    float pow= (float) Math.pow((1+rate),year*MONTH);
+    //int total=(int)((rate*pow)/pow-1);
+    mortgage=principal*((rate*pow)/(pow-1));
+    String total= NumberFormat.getCurrencyInstance().format(mortgage);
+    System.out.println("Mortgage:"+total);
+
+
+}
+
 }
